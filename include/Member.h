af4e8cea6c992f59c9ce1900e2a818355cf81103
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-12 19:12:29 +0300

    Update Member.h

--- include/Member.h
+++ include/Member.h
@@ -1,1 +1,24 @@
+#pragma once
+#include <string>
+#include <memory>
+
+class Member {
+public:
+    Member(std::string name, int id);
+    virtual ~Member();
+
+    Member(const Member& other);
+    Member& operator=(Member other);
+
+    virtual std::unique_ptr<Member> clone() const = 0;
+    virtual void info() const = 0;
+
+    int getID() const noexcept;
+    const std::string& getName() const noexcept;
+
+    friend void swap(Member& a, Member& b) noexcept;
+protected:
+    std::string name_;
+    int id_;
+};
 
