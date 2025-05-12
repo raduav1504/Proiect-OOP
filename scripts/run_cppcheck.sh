10fd5017cdb6ec30ca55ead207af214af62809dc
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-12 22:32:36 +0300

    Update run_cppcheck.sh

--- scripts/run_cppcheck.sh
+++ scripts/run_cppcheck.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/bash
 
-CPPCHECK_INCLUDE="-I ${PWD}/include"
-
-cppcheck \
-    ${CPPCHECK_INCLUDE} \
-    --enable=all \
+cppcheck --enable=all \
     --inline-suppr \
     --project="${BUILD_DIR:-build}"/compile_commands.json \
-    -i"${BUILD_DIR:-build}"   --suppress="*:${BUILD_DIR:-build}/*" \
-    -i"${EXT_DIR:-ext}"       --suppress="*:${EXT_DIR:-ext}/*" \
+    -i"${BUILD_DIR:-build}" --suppress="*:${BUILD_DIR:-build}/*" \
+    -i"${EXT_DIR:-ext}" --suppress="*:${EXT_DIR:-ext}/*" \
     -i"${GEN_DIR:-generated}" --suppress="*:${GEN_DIR:-generated}/*" \
     --suppress=missingIncludeSystem \
-    --suppress=missingInclude \
     --suppress=unmatchedSuppression \
     --suppress=useStlAlgorithm \
     --check-level=exhaustive \
