53a31b059515aa98fa4ed0bf8356c1adb198ca4e
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-12 19:11:36 +0300

    Update Exception.h

--- include/Exception.h
+++ include/Exception.h
@@ -1,1 +1,17 @@
+#pragma once
+#include <exception>
+#include <string>
+
+class GymException : public std::exception {
+public:
+    explicit GymException(std::string msg) noexcept;
+    const char* what() const noexcept override;
+private:
+    std::string message_;
+};
+
+class IndexException : public GymException {
+public:
+    IndexException(const std::string& who, int idx) noexcept;
+};
 
