21be3855fbe1d34d81b3c0b9692546c786feed85
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-12 22:38:34 +0300

    Delete include/Report.h

--- include/Report.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <vector>
-#include <memory>
-#include "FacilityElement.h"
-
-class Report {
-public:
-    static void facilityReport(const std::vector<std::unique_ptr<FacilityElement>>& elems);
-};
