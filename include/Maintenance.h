b34631caf589f4543cdc7cfa8e4e40f26d4aa1f7
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-12 20:02:44 +0300

    Update Maintenance.h

--- include/Maintenance.h
+++ include/Maintenance.h
@@ -5,10 +5,10 @@ class MaintenanceRequest {
 public:
     MaintenanceRequest(std::string equipmentType, int equipmentId, std::string date);
     const std::string& getEquipmentType() const noexcept;
-    int getEquipmentId() const noexcept;
-    const std::string& getDate() const noexcept;
+    int                 getEquipmentId()   const noexcept;
+    const std::string& getDate()           const noexcept;
 private:
     std::string equipmentType_;
-    int equipmentId_;
-    std::string date_;  // Simplu, YYYY-MM-DD
+    int         equipmentId_;
+    std::string date_;
 };
