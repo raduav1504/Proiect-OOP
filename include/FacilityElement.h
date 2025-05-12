12373842d65a17e5df0095af8556bd50d917560d
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-12 21:34:15 +0300

    Create FacilityElement.h

---
+++ include/FacilityElement.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <string>
+#include <memory>
+#include <ostream>
+
+class FacilityElement {
+public:
+    FacilityElement(std::string name);
+    virtual ~FacilityElement();
+    FacilityElement(const FacilityElement& other);
+    FacilityElement& operator=(FacilityElement other);
+    friend void swap(FacilityElement& a, FacilityElement& b) noexcept;
+
+    // non-virtual
+    void print(std::ostream& os) const;
+
+    // virtual
+    virtual void describe() const = 0;
+    virtual std::unique_ptr<FacilityElement> clone() const = 0;
+
+    const std::string& getName() const noexcept;
+
+protected:
+    std::string name_;
+};
+
+std::ostream& operator<<(std::ostream& os, const FacilityElement& elem);
