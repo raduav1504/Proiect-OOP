18413eb308e607d743f791eb6f9f75cb78c9951c
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-12 22:12:05 +0300

    Update run_cppcheck.sh

--- scripts/run_cppcheck.sh
+++ scripts/run_cppcheck.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-cppcheck --enable=all \
+cppcheck -I include --enable=all \
     --inline-suppr \
     --project="${BUILD_DIR:-build}"/compile_commands.json \
     -i"${BUILD_DIR:-build}" --suppress="*:${BUILD_DIR:-build}/*" \
