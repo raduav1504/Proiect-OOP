c39965ef13f0431b14e554a049780fad83fa2d82
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-12 21:35:54 +0300

    Update Elliptical.h

--- include/Elliptical.h
+++ include/Elliptical.h
@@ -4,7 +4,8 @@
 class Elliptical : public Equipment {
 public:
     Elliptical();
-    void update() override;
+    void describe()   const override;
+    void update()          override;
     void startUsage(int duration, const std::string& user) override;
-    std::unique_ptr<Equipment> clone() const override;
+    std::unique_ptr<FacilityElement> clone() const override;
 };
