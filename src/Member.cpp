d3282d091ea83ae26a5b3b7d648946238d3410cf
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-12 20:04:34 +0300

    Update Member.cpp

--- src/Member.cpp
+++ src/Member.cpp
@@ -1,43 +1,36 @@
 #include "Member.h"
 #include <iostream>
-#include <utility>
+
+int Member::totalCount_ = 0;
 
 Member::Member(std::string name, int id)
-  : name_{std::move(name)}, id_{id} {
+  : name_{std::move(name)}, id_{id}
+{
+    ++totalCount_;
     std::cout << "New member: " << name_ << " (ID " << id_ << ")\n";
 }
 
-Member::~Member() = default;
+Member::~Member() {
+    --totalCount_;
+}
 
 Member::Member(const Member& other)
   : name_{other.name_}, id_{other.id_}
-{}
+{
+    ++totalCount_;
+}
 
 Member& Member::operator=(Member other) {
     swap(*this, other);
     return *this;
 }
 
+int Member::getID() const noexcept      { return id_; }
+const std::string& Member::getName() const noexcept { return name_; }
+int Member::getTotalMembers() noexcept  { return totalCount_; }
+
 void swap(Member& a, Member& b) noexcept {
     using std::swap;
     swap(a.name_, b.name_);
-    swap(a.id_, b.id_);
+    swap(a.id_,   b.id_);
 }
-
-int Member::getID() const noexcept { return id_; }
-const std::string& Member::getName() const noexcept { return name_; }
-
-// Exemplu subclasă concretă
-class Regular : public Member {
-public:
-    Regular(const std::string& name, int id)
-      : Member(name, id) {}
-    std::unique_ptr<Member> clone() const override {
-        return std::make_unique<Regular>(*this);
-    }
-    void info() const override {
-        std::cout << "Regular Member: " << name_
-                  << " (ID " << id_ << ")\n";
-    }
-};
-
