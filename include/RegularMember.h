00f54cd9507055017d9e118ad7ecaf37354296b3
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-12 22:38:40 +0300

    Delete include/RegularMember.h

--- include/RegularMember.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Member.h"
-
-class RegularMember : public Member {
-public:
-    RegularMember(const std::string& name, int id);
-    std::unique_ptr<Member> clone() const override;
-    void info() const override;
-};
