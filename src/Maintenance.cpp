61a9bb02ddb57e4ade00d9e42e4f54f9e9928884
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-12 20:05:05 +0300

    Update Maintenance.cpp

--- src/Maintenance.cpp
+++ src/Maintenance.cpp
@@ -1,1 +1,12 @@
+#include "Maintenance.h"
+
+MaintenanceRequest::MaintenanceRequest(std::string equipmentType, int equipmentId, std::string date)
+  : equipmentType_{std::move(equipmentType)}
+  , equipmentId_{equipmentId}
+  , date_{std::move(date)}
+{}
+
+const std::string& MaintenanceRequest::getEquipmentType() const noexcept { return equipmentType_; }
+int                 MaintenanceRequest::getEquipmentId()   const noexcept { return equipmentId_; }
+const std::string& MaintenanceRequest::getDate()           const noexcept { return date_; }
 
