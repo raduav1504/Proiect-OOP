7cac04ed0b469fcd91fc7c6343a5b08e5e4ae55b
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-12 21:35:42 +0300

    Update Treadmill.h

--- include/Treadmill.h
+++ include/Treadmill.h
@@ -4,7 +4,8 @@
 class Treadmill : public Equipment {
 public:
     Treadmill();
-    void update() override;
+    void describe()   const override;
+    void update()          override;
     void startUsage(int duration, const std::string& user) override;
-    std::unique_ptr<Equipment> clone() const override;
+    std::unique_ptr<FacilityElement> clone() const override;
 };
