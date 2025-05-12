edb620349d5f922d2cd58450416dcf9515004f3b
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-12 21:46:43 +0300

    Update StationaryBike.cpp

--- src/StationaryBike.cpp
+++ src/StationaryBike.cpp
@@ -1,41 +1,29 @@
 #include "StationaryBike.h"
 #include <iostream>
 
-StationaryBike::StationaryBike()
-  : Equipment("Stationary Bike")
-{}
+StationaryBike::StationaryBike() : Equipment("Stationary Bike") {}
+
+void StationaryBike::describe() const {
+    std::cout << "Stationary bike\n";
+}
 
 void StationaryBike::update() {
     if (underMaintenance_) {
-        if (--maintenanceRemaining_ == 0) {
-            completeMaintenance();
-        }
+        if (--maintenanceRemaining_ == 0) completeMaintenance();
         return;
     }
     if (inUse_ && --remaining_ == 0) {
-        inUse_ = false;
-        user_.clear();
-        std::cout << type_ << " now available\n";
+        inUse_ = false; user_.clear();
+        std::cout << name_ << " now available\n";
     }
 }
 
 void StationaryBike::startUsage(int duration, const std::string& user) {
-    if (underMaintenance_) {
-        std::cout << type_ << " is under maintenance\n";
-        return;
-    }
-    if (inUse_) {
-        std::cout << type_ << " already in use by " << user_ << "\n";
-        return;
-    }
-    inUse_      = true;
-    remaining_  = duration;
-    user_       = user;
-    ++usageCount_;
-    std::cout << "Started " << type_
-              << " for " << user << " (" << duration << ")\n";
+    if (underMaintenance_ || inUse_) return;
+    inUse_ = true; remaining_ = duration; user_ = user; ++usageCount_;
+    std::cout << user << " started " << name_ << "\n";
 }
 
-std::unique_ptr<Equipment> StationaryBike::clone() const {
+std::unique_ptr<FacilityElement> StationaryBike::clone() const {
     return std::make_unique<StationaryBike>(*this);
 }
