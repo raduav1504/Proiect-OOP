7c44f7eb351f9f1ecd71016df3c60d070426e030
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-12 20:03:57 +0300

    Create StationaryBike.cpp

---
+++ src/StationaryBike.cpp
@@ -0,0 +1,41 @@
+#include "StationaryBike.h"
+#include <iostream>
+
+StationaryBike::StationaryBike()
+  : Equipment("Stationary Bike")
+{}
+
+void StationaryBike::update() {
+    if (underMaintenance_) {
+        if (--maintenanceRemaining_ == 0) {
+            completeMaintenance();
+        }
+        return;
+    }
+    if (inUse_ && --remaining_ == 0) {
+        inUse_ = false;
+        user_.clear();
+        std::cout << type_ << " now available\n";
+    }
+}
+
+void StationaryBike::startUsage(int duration, const std::string& user) {
+    if (underMaintenance_) {
+        std::cout << type_ << " is under maintenance\n";
+        return;
+    }
+    if (inUse_) {
+        std::cout << type_ << " already in use by " << user_ << "\n";
+        return;
+    }
+    inUse_      = true;
+    remaining_  = duration;
+    user_       = user;
+    ++usageCount_;
+    std::cout << "Started " << type_
+              << " for " << user << " (" << duration << ")\n";
+}
+
+std::unique_ptr<Equipment> StationaryBike::clone() const {
+    return std::make_unique<StationaryBike>(*this);
+}
