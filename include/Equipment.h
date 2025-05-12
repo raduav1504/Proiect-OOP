31a3c647be10766576e7096bd79c83db86a0e06a
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-12 19:59:07 +0300

    Update Equipment.h

--- include/Equipment.h
+++ include/Equipment.h
@@ -1,33 +1,49 @@
 #pragma once
 #include <string>
 #include <memory>
+#include <ostream>
 
 class Equipment {
 public:
     Equipment(std::string type);
     virtual ~Equipment();
 
-    // Copy & swap
     Equipment(const Equipment& other);
     Equipment& operator=(Equipment other);
 
-    // Polimorfism
+    // polymorphic interface
     virtual void update() = 0;
     virtual void startUsage(int duration, const std::string& user) = 0;
     virtual std::unique_ptr<Equipment> clone() const = 0;
 
-    const std::string& getType() const noexcept;
-    int getUsageCount() const noexcept;
-    bool isUnderMaintenance() const noexcept;
-    void scheduleMaintenance();
+    // maintenance
+    void scheduleMaintenance(int duration);
     void completeMaintenance();
 
+    // status queries
+    const std::string& getType()        const noexcept;
+    int                 getUsageCount() const noexcept;
+    bool                isUnderMaintenance() const noexcept;
+    bool                isInUse()      const noexcept;
+
+    // static tracking
+    static int getTotalEquipmentCount() noexcept;
+
+    // swap for copy-and-swap
     friend void swap(Equipment& a, Equipment& b) noexcept;
+
+    // pretty-print
+    friend std::ostream& operator<<(std::ostream& os, const Equipment& eq);
+
 protected:
     std::string type_;
-    bool inUse_;
-    int remaining_;
+    bool        inUse_;
+    int         remaining_;
     std::string user_;
-    int usageCount_;
-    bool underMaintenance_;
+    int         usageCount_;
+    bool        underMaintenance_;
+    int         maintenanceRemaining_;
+
+private:
+    static int totalCount_;
 };
