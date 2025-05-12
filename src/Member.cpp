9cf0292ae0c1231495da468aac3a436507043433
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-12 21:45:39 +0300

    Update Member.cpp

--- src/Member.cpp
+++ src/Member.cpp
@@ -1,5 +1,4 @@
 #include "Member.h"
-#include <iostream>
 
 int Member::totalCount_ = 0;
 
@@ -7,7 +6,6 @@ Member::Member(std::string name, int id)
   : name_{std::move(name)}, id_{id}
 {
     ++totalCount_;
-    std::cout << "New member: " << name_ << " (ID " << id_ << ")\n";
 }
 
 Member::~Member() {
@@ -25,12 +23,17 @@ Member& Member::operator=(Member other) {
     return *this;
 }
 
-int Member::getID() const noexcept      { return id_; }
-const std::string& Member::getName() const noexcept { return name_; }
-int Member::getTotalMembers() noexcept  { return totalCount_; }
-
 void swap(Member& a, Member& b) noexcept {
     using std::swap;
     swap(a.name_, b.name_);
     swap(a.id_,   b.id_);
+}
+
+int Member::getID() const noexcept         { return id_; }
+const std::string& Member::getName() const noexcept { return name_; }
+int Member::getTotalCount() noexcept       { return totalCount_; }
+
+std::ostream& operator<<(std::ostream& os, const Member& m) {
+    m.info();
+    return os;
 }
