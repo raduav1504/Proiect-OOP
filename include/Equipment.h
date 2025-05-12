19f602ec8faed0323c0abe172d35ddde1f859fdb
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-12 19:11:50 +0300

    Update Equipment.h

--- include/Equipment.h
+++ include/Equipment.h
@@ -1,1 +1,28 @@
+#pragma once
+#include <string>
+#include <memory>
+
+class Equipment {
+public:
+    Equipment(std::string type);
+    virtual ~Equipment();
+
+    // Copy & swap
+    Equipment(const Equipment& other);
+    Equipment& operator=(Equipment other);
+
+    // Polimorfism
+    virtual void update() = 0;
+    virtual void startUsage(int duration, const std::string& user) = 0;
+    virtual std::unique_ptr<Equipment> clone() const = 0;
+
+    const std::string& getType() const noexcept;
+
+    friend void swap(Equipment& a, Equipment& b) noexcept;
+protected:
+    std::string type_;
+    bool inUse_;
+    int remaining_;
+    std::string user_;
+};
 
