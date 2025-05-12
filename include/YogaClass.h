47bb057e51394f7e0e22763022476d39eae44b40
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-12 21:36:48 +0300

    Create YogaClass.h

---
+++ include/YogaClass.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "FitnessClass.h"
+
+class YogaClass : public FitnessClass {
+public:
+    YogaClass(int duration);
+    void describe()   const override;
+    void attend(const std::string& memberName) override;
+    void update()                 override;
+    std::unique_ptr<FacilityElement> clone() const override;
+};
