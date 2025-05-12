5c18a5f651e8d69f347d20cdac808043da26354c
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-12 20:04:11 +0300

    Update Exception.cpp

--- src/Exception.cpp
+++ src/Exception.cpp
@@ -1,12 +1,13 @@
 #include "Exception.h"
 
 GymException::GymException(std::string msg) noexcept
-  : message_{std::move(msg)} {}
+  : message_{std::move(msg)}
+{}
 
 const char* GymException::what() const noexcept {
     return message_.c_str();
 }
 
 IndexException::IndexException(const std::string& who, int idx) noexcept
-  : GymException(who + " index invalid: " + std::to_string(idx)) {}
-
+  : GymException(who + " index invalid: " + std::to_string(idx))
+{}
