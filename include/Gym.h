4905e591f43de220efe4a8a508ddc11d8a71c7ce
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-12 19:13:36 +0300

    Update Gym.h

--- include/Gym.h
+++ include/Gym.h
@@ -1,1 +1,33 @@
+#pragma once
+#include <vector>
+#include <memory>
+#include "Equipment.h"
+#include "Member.h"
+#include "Exception.h"
+
+class Gym {
+public:
+    Gym(std::string name);
+    ~Gym();
+
+    Gym(const Gym& other);
+    Gym& operator=(Gym other);
+
+    void addEquipment(const Equipment& eq);
+    void addMember(const Member& m);
+
+    void updateAll();
+    void startUsage(size_t eqIdx, size_t memIdx, int duration);
+
+    void printStatus() const;
+
+    static int totalGyms() noexcept;
+private:
+    std::string name_;
+    std::vector<std::unique_ptr<Equipment>> equipments_;
+    std::vector<std::unique_ptr<Member>> members_;
+
+    static int gymCount_;
+    friend void swap(Gym& a, Gym& b) noexcept;
+};
 
