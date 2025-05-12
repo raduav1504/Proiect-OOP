3246a32ae028d39f3233b6df27c2bd914659b847
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-12 22:35:00 +0300

    Update Member.cpp

--- src/Member.cpp
+++ src/Member.cpp
@@ -1,39 +1,30 @@
-#include "Member.h"
+#include "Member.hpp"
+#include <utility>
 
-int Member::totalCount_ = 0;
+int Member::nextId_ = 1;
 
-Member::Member(std::string name, int id)
-  : name_{std::move(name)}, id_{id}
-{
-    ++totalCount_;
-}
-
-Member::~Member() {
-    --totalCount_;
-}
+Member::Member(std::string name)
+  : id_(nextId_++), name_(std::move(name))
+{}
 
 Member::Member(const Member& other)
-  : name_{other.name_}, id_{other.id_}
-{
-    ++totalCount_;
-}
+  : id_(nextId_++), name_(other.name_)
+{}
 
 Member& Member::operator=(Member other) {
     swap(*this, other);
     return *this;
 }
 
-void swap(Member& a, Member& b) noexcept {
+void swap(Member& a, Member& b) {
     using std::swap;
-    swap(a.name_, b.name_);
-    swap(a.id_,   b.id_);
+    swap(a.id_,    b.id_);
+    swap(a.name_,  b.name_);
 }
 
-int Member::getID() const noexcept         { return id_; }
-const std::string& Member::getName() const noexcept { return name_; }
-int Member::getTotalCount() noexcept       { return totalCount_; }
+int Member::getId() const { return id_; }
+const std::string& Member::getName() const { return name_; }
 
-std::ostream& operator<<(std::ostream& os, const Member& m) {
-    m.info();
-    return os;
+void Member::print(std::ostream& os) const {
+    os << "Member[" << id_ << "] " << name_;
 }
