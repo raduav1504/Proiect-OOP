fa45e02357e3f7442f7c29a6a81f5892be276365
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-12 22:36:54 +0300

    Update Treadmill.cpp

--- src/Treadmill.cpp
+++ src/Treadmill.cpp
@@ -1,29 +1,13 @@
-#include "Treadmill.h"
-#include <iostream>
+#include "Treadmill.hpp"
 
-Treadmill::Treadmill() : Equipment("Treadmill") {}
+Treadmill::Treadmill(double maxSpeed)
+  : Equipment("Treadmill"), maxSpeed_(maxSpeed)
+{}
 
-void Treadmill::describe() const {
-    std::cout << "Treadmill for cardio\n";
-}
-
-void Treadmill::update() {
-    if (underMaintenance_) {
-        if (--maintenanceRemaining_ == 0) completeMaintenance();
-        return;
-    }
-    if (inUse_ && --remaining_ == 0) {
-        inUse_ = false; user_.clear();
-        std::cout << name_ << " now available\n";
-    }
-}
-
-void Treadmill::startUsage(int duration, const std::string& user) {
-    if (underMaintenance_ || inUse_) return;
-    inUse_ = true; remaining_ = duration; user_ = user; ++usageCount_;
-    std::cout << user << " started " << name_ << "\n";
-}
-
-std::unique_ptr<FacilityElement> Treadmill::clone() const {
+std::unique_ptr<Equipment> Treadmill::clone() const {
     return std::make_unique<Treadmill>(*this);
+}
+
+void Treadmill::printDetails(std::ostream& os) const {
+    os << "  [MaxSpeed: " << maxSpeed_ << " km/h]";
 }
