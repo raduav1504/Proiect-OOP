2b89dd4e11e72144fad2442524bcc2aac5c8f9a8
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-12 22:35:39 +0300

    Update StationaryBike.cpp

--- src/StationaryBike.cpp
+++ src/StationaryBike.cpp
@@ -1,29 +1,13 @@
-#include "StationaryBike.h"
-#include <iostream>
+#include "StationaryBike.hpp"
 
-StationaryBike::StationaryBike() : Equipment("Stationary Bike") {}
+StationaryBike::StationaryBike(int resistance)
+  : Equipment("StationaryBike"), resistance_(resistance)
+{}
 
-void StationaryBike::describe() const {
-    std::cout << "Stationary bike\n";
-}
-
-void StationaryBike::update() {
-    if (underMaintenance_) {
-        if (--maintenanceRemaining_ == 0) completeMaintenance();
-        return;
-    }
-    if (inUse_ && --remaining_ == 0) {
-        inUse_ = false; user_.clear();
-        std::cout << name_ << " now available\n";
-    }
-}
-
-void StationaryBike::startUsage(int duration, const std::string& user) {
-    if (underMaintenance_ || inUse_) return;
-    inUse_ = true; remaining_ = duration; user_ = user; ++usageCount_;
-    std::cout << user << " started " << name_ << "\n";
-}
-
-std::unique_ptr<FacilityElement> StationaryBike::clone() const {
+std::unique_ptr<Equipment> StationaryBike::clone() const {
     return std::make_unique<StationaryBike>(*this);
+}
+
+void StationaryBike::printDetails(std::ostream& os) const {
+    os << "  [Resistance: level " << resistance_ << "]";
 }
