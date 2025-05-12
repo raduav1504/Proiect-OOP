6062eb75ce7d078cae2fb1ba7bedef883f8911f9
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-12 22:40:54 +0300

    Delete include/GuestMember.h

--- include/GuestMember.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Member.h"
-
-class GuestMember : public Member {
-public:
-    GuestMember(const std::string& name, int id);
-    std::unique_ptr<Member> clone() const override;
-    void info() const override;
-};
