2375d86e73bf1ca0ec89de17e52b23b19528b49d
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-12 20:04:56 +0300

    Create RegularMember.cpp

---
+++ src/RegularMember.cpp
@@ -0,0 +1,15 @@
+#include "RegularMember.h"
+#include <iostream>
+
+RegularMember::RegularMember(const std::string& name, int id)
+  : Member(name, id)
+{}
+
+std::unique_ptr<Member> RegularMember::clone() const {
+    return std::make_unique<RegularMember>(*this);
+}
+
+void RegularMember::info() const {
+    std::cout << "Regular Member: " << name_
+              << " (ID " << id_ << ")\n";
+}
