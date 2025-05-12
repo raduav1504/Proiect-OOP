648d8a1f9031f5e93dabff5daf98f202d94deb78
raduav1504 <radu.avadanei.ichb@gmail.com>
2025-05-12 22:38:47 +0300

    Delete include/PremiumMember.h

--- include/PremiumMember.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Member.h"
-
-class PremiumMember : public Member {
-public:
-    PremiumMember(const std::string& name, int id);
-    std::unique_ptr<Member> clone() const override;
-    void info() const override;
-};
